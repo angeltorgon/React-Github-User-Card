91871177ea6ef947ea7f37054cbe694b97664337
angel-torres <alas4224@gmail.com>
2019-12-18 22:14:55 -0600

    finished making axios call to get user information from Github api; was able to render user information on screen

---
+++ client/src/App.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import axios from 'axios';
+import './App.css';
+
+import { Card } from '@material-ui/core'
+
+class App extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      user: {}
+    }
+  }
+
+  componentDidMount() {
+    axios.get('https://api.github.com/users/angel-torres')
+    .then((res) => {
+      console.log(res.data)
+      this.setState({
+        user: res.data
+      }) 
+    }).catch(error => {
+      console.log(error)  
+    })
+
+  }
+
+  render() {
+    return (
+      <Card className="App">
+        <img src={this.state.user.avatar_url} />
+        <h1>{this.state.user.login}</h1>
+        <p>{this.state.user.bio}</p>
+        <p>Followers: {this.state.user.followers}</p>
+        <p>Following: {this.state.user.following}</p>
+      </Card>
+    );
+  }
+}
+
+export default App;
