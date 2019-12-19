33fa3fe68ac81149d1ed7110dacd24305b2945d7
angel-torres <alas4224@gmail.com>
2019-12-19 00:18:00 -0600

    finished adding search functionality to app so that you can search different github handles

--- client/src/App.js
+++ client/src/App.js
@@ -8,7 +8,8 @@ class App extends React.Component {
   constructor(){
     super();
     this.state = {
-      user: {}
+      user: {},
+      github: ""
     }
   }
 
@@ -16,23 +17,52 @@ class App extends React.Component {
     axios.get('https://api.github.com/users/angel-torres')
     .then((res) => {
       this.setState({
+        ...this.state,
         user: res.data
       }) 
     }).catch(error => {
       console.log(error)  
     })
+  }
 
+  handleChanges = (e) => {
+      console.log(e.target.value)
+      console.log(e.target.name)
+      e.preventDefault();
+      this.setState({...this.state, [e.target.name]: e.target.value});
+    }
+
+  handleSearch = (e) => {
+    e.preventDefault()
+    axios.get(`https://api.github.com/users/${this.state.github}`)
+    .then((res) => {
+      this.setState({
+        ...this.state,
+        user: res.data
+      }) 
+    }).catch(error => {
+      console.log(error)  
+    })
   }
 
   render() {
     return (
-      <Card className="App">
-        <img src={this.state.user.avatar_url} />
-        <h1>{this.state.user.login}</h1>
-        <p>{this.state.user.bio}</p>
-        <p>Followers: {this.state.user.followers}</p>
-        <p>Following: {this.state.user.following}</p>
-      </Card>
+			<div>
+				<Card className="App">
+				<img src={this.state.user.avatar_url} />
+				<h1>{this.state.user.login}</h1>
+				<p>{this.state.user.bio}</p>
+				<p>Followers: {this.state.user.followers}</p>
+				<p>Following: {this.state.user.following}</p>
+				</Card>
+				<Card className="App">
+                    <form onSubmit={this.handleSearch}>
+							<input name="github" type="texts" onChange={this.handleChanges} placeholder="github handle"/>
+					<button type="submit">SEARCH</button>
+                    </form>
+				</Card>
+						
+			</div>
     );
   }
 }
